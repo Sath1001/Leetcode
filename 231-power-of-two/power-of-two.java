eb7902e912f8396f086310a4ba2fb482e081031b
Sath1001 <155250421+Sath1001@users.noreply.github.com>
2025-07-23 19:47:16 +0530

    Time: 1 ms (35.01%) | Memory: 41 MB (36.62%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        // Base cases
+        if (n == 1) return true;    // 2^0 = 1
+        if (n <= 0 || n % 2 != 0) return false;
+
+        // Recursive step: keep dividing by 2
+        return isPowerOfTwo(n / 2);
     }
 }
