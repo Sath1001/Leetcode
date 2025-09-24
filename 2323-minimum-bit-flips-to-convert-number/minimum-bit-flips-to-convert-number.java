5694ca4e524e4426ce888a8c56ede5eb49db9b73
Sath1001 <155250421+Sath1001@users.noreply.github.com>
2025-09-24 14:21:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (15.57%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int a, int b) {
+        int n=a^b;
+        int c=0;
+        while(n!=0){
+            n=(n&(n-1));
+            c++;
+         } 
+        return c;
+
+
+    }
+}
